f03d4bb1aef78216759db1fadf9d8e770e5264c9
Zarino Zappia <mail@zarino.co.uk>
2015-01-30 11:08:25 +0000

    slightly hacky but nonetheless endearing parallax background images

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,144 @@ $.fn.isVisible = function(options){
   }
 }
 
+var makeSparkline = function makeSparkline(){
+  // expects $(this) to be a table with a single row of cells
+  var $table = $(this);
+  var $canvas = $('<canvas>').insertBefore($table);
+  var data = [];
+  var labels = [];
+  var strokeColour = $(this).attr('data-stroke') || 'rgba(255, 255, 255, 0.5)';
+  var fillColour = $(this).attr('data-fill') || 'rgba(255, 255, 255, 0.1)';
+  var pointColour = $(this).attr('data-points') || '#fff';
+  var pointStrokeColour = $('body').css('background-color');
+  var settings = {
+    animation: false,
+    bezierCurve: false,
+    scaleShowGridLines: false,
+    showScale: false,
+    responsive: true,
+    maintainAspectRatio: false,
+    showTooltips: false
+  };
+
+  $('td', $table).each(function(i){
+    data.push(parseFloat($(this).text()));
+    labels.push('');
+  });
+
+  if($(this).attr('data-start')){
+    settings.scaleOverride = true;
+    settings.scaleStartValue = parseFloat($(this).attr('data-start'));
+    if($(this).attr('data-end')){
+      settings.scaleSteps = parseFloat($(this).attr('data-end')) - settings.scaleStartValue;
+    } else {
+      settings.scaleSteps = data[data.length - 1] - settings.scaleStartValue;
+    }
+    settings.scaleStepWidth = 1;
+  }
+
+  new Chart($canvas.get(0).getContext('2d')).Line({
+    labels: labels,
+    datasets: [{
+      fillColor: fillColour,
+      strokeColor: strokeColour,
+      pointColor: pointColour,
+      pointStrokeColor: pointStrokeColour,
+      data: data
+    }]
+  }, settings);
+
+  $table.hide();
+}
+
+var makePieChart = function makePieChart(){
+  // expects $(this) to be a table with one row of two cells (label, value) per pie segment
+  var $table = $(this);
+  var $rows = $('tr', $table);
+  var $canvas = $('<canvas>').attr({
+    width: 100,
+    height: 100
+  }).insertBefore($table);
+
+  var animationDelay = -1;
+  if((typeof $table.attr('data-delay') != 'undefined') && parseInt($table.attr('data-delay')) > -1){
+    animationDelay = parseInt($table.attr('data-delay'));
+  }
+
+  var data = [];
+  var opacities = [0.4, 0.6, 0.8, 0.4, 0.6, 0.8];
+  var settings = {
+    responsive: true,
+    segmentStrokeColor: $('body').css('background-color'),
+    animationEasing: 'easeOutQuart',
+    tooltipFontSize: 18,
+    tooltipXPadding: 9,
+    tooltipYPadding: 9,
+    tooltipTemplate: '<%=label%>'
+  };
+
+  $rows.each(function(i){
+    data.push({
+      value: parseFloat($('td', this).text()),
+      label: $('th', this).text(),
+      color: 'rgba(255, 255, 255, ' + opacities[i] + ')',
+      highlight: 'rgba(255, 255, 255, 0.9)',
+    })
+  });
+
+  var chart = new Chart($canvas.get(0).getContext('2d')).Pie(data, settings);
+  $table.hide();
+
+  if(animationDelay > -1) {
+    var chartHasRendered = false;
+    $canvas.css('visibility', 'hidden');
+
+    $(window).scroll(function(){
+      if($canvas.isVisible() && chartHasRendered == false){
+        chartHasRendered = true;
+        setTimeout(function(){
+          $canvas.css('visibility', '');
+          chart.render()
+        }, animationDelay);
+      }
+    });
+  }
+}
+
+var setUpParallaxBackground = function setUpParallaxBackground(){
+  var $el = $(this);
+  var backgroundY = parseFloat($el.css('background-position-y')) / 100;
+  var elHeight = $el.height();
+  var elTop = $el.offset().top;
+  var elMiddle = elTop + (elHeight / 2);
+  var elBottom = elTop + elHeight;
+
+  var repositionBackground = function(){
+    var wHeight = $(window).height();
+    var wTop = $(window).scrollTop();
+    var wMiddle = wTop + (wHeight / 2);
+    var wBottom = wTop + wHeight;
+
+    // some pixel value (negative above the middle of the window, positive below)
+    var elMiddleOffset = elMiddle - wMiddle;
+
+    // fraction representing how negative or positive that midpoint is, compared to window bounds
+    var elMiddleOffsetFraction = elMiddleOffset / wHeight;
+
+    // adjust default position by adding/subtracting a bit of the midpoint offset
+    var newBackgroundY = backgroundY + (elMiddleOffsetFraction / 3);
+
+    // Make sure background position is between 0 and 100
+    var newBackgroundYBounded = Math.max(Math.min(newBackgroundY * 100, 100), 0);
+
+    $el.css('background-position-y', newBackgroundYBounded + '%');
+  }
+
+  $(window).scroll(repositionBackground);
+  $(window).on('resize', repositionBackground);
+  repositionBackground();
+}
+
 $(function(){
   $('body').addClass('has-js');
   $('.nav-toggle').on('click', function(){
@@ -23,109 +161,9 @@ $(function(){
     $(this).toggleClass('nav-opened');
   });
 
-  $('.js-make-sparkline').each(function(){
-    // expects $(this) to be a table with a single row of cells
-    var $table = $(this);
-    var $canvas = $('<canvas>').insertBefore($table);
-    var data = [];
-    var labels = [];
-    var strokeColour = $(this).attr('data-stroke') || 'rgba(255, 255, 255, 0.5)';
-    var fillColour = $(this).attr('data-fill') || 'rgba(255, 255, 255, 0.1)';
-    var pointColour = $(this).attr('data-points') || '#fff';
-    var pointStrokeColour = $('body').css('background-color');
-    var settings = {
-      animation: false,
-      bezierCurve: false,
-      scaleShowGridLines: false,
-      showScale: false,
-      responsive: true,
-      maintainAspectRatio: false,
-      showTooltips: false
-    };
+  $('.js-make-sparkline').each(makeSparkline);
+  $('.js-make-pie-chart').each(makePieChart);
 
-    $('td', $table).each(function(i){
-      data.push(parseFloat($(this).text()));
-      labels.push('');
-    });
-
-    if($(this).attr('data-start')){
-      settings.scaleOverride = true;
-      settings.scaleStartValue = parseFloat($(this).attr('data-start'));
-      if($(this).attr('data-end')){
-        settings.scaleSteps = parseFloat($(this).attr('data-end')) - settings.scaleStartValue;
-      } else {
-        settings.scaleSteps = data[data.length - 1] - settings.scaleStartValue;
-      }
-      settings.scaleStepWidth = 1;
-    }
-
-    new Chart($canvas.get(0).getContext('2d')).Line({
-      labels: labels,
-      datasets: [{
-        fillColor: fillColour,
-        strokeColor: strokeColour,
-        pointColor: pointColour,
-        pointStrokeColor: pointStrokeColour,
-        data: data
-      }]
-    }, settings);
-
-    $table.hide();
-  });
-
-  $('.js-make-pie-chart').each(function(){
-    // expects $(this) to be a table with one row of two cells (label, value) per pie segment
-    var $table = $(this);
-    var $rows = $('tr', $table);
-    var $canvas = $('<canvas>').attr({
-      width: 100,
-      height: 100
-    }).insertBefore($table);
-
-    var animationDelay = -1;
-    if((typeof $table.attr('data-delay') != 'undefined') && parseInt($table.attr('data-delay')) > -1){
-      animationDelay = parseInt($table.attr('data-delay'));
-    }
-
-    var data = [];
-    var opacities = [0.4, 0.6, 0.8, 0.4, 0.6, 0.8];
-    var settings = {
-      responsive: true,
-      segmentStrokeColor: $('body').css('background-color'),
-      animationEasing: 'easeOutQuart',
-      tooltipFontSize: 18,
-      tooltipXPadding: 9,
-      tooltipYPadding: 9,
-      tooltipTemplate: '<%=label%>'
-    };
-
-    $rows.each(function(i){
-      data.push({
-        value: parseFloat($('td', this).text()),
-        label: $('th', this).text(),
-        color: 'rgba(255, 255, 255, ' + opacities[i] + ')',
-        highlight: 'rgba(255, 255, 255, 0.9)',
-      })
-    });
-
-    var chart = new Chart($canvas.get(0).getContext('2d')).Pie(data, settings);
-    $table.hide();
-
-    if(animationDelay > -1) {
-      var chartHasRendered = false;
-      $canvas.css('visibility', 'hidden');
-
-      $(window).scroll(function(){
-        if($canvas.isVisible() && chartHasRendered == false){
-          chartHasRendered = true;
-          setTimeout(function(){
-            $canvas.css('visibility', '');
-            chart.render()
-          }, animationDelay);
-        }
-      });
-    }
-
-  });
+  $('.js-parallax-background').each(setUpParallaxBackground);
 
 });
