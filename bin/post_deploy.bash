cbc4421137a7806355b7eddccbcbe9f6bbe7cd7c
Zarino Zappia <mail@zarino.co.uk>
2015-10-22 17:27:23 +0100

    Most content for 2015 annual report
    Some images and figures still need updating, but this is
    the majority of it.

--- bin/post_deploy.bash
+++ bin/post_deploy.bash
@@ -1,6 +1,6 @@
 cd "$(dirname $BASH_SOURCE)"/..
 
-ruby1.9.1 /usr/bin/bundle install --deployment --binstubs vendor/bin
-cd 2014
-../vendor/bin/sass --update assets/sass:assets/css
-../vendor/bin/jekyll build
+ruby1.9.1 /usr/bin/bundle install --deployment --binstubs ../gem-bin
+cd $1
+../../gem-bin/sass --update assets/sass:assets/css
+../../gem-bin/jekyll build
