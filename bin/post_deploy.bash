0d8635bf0895be4e92ac841bea0a7cfe5fcd4f2e
Zarino Zappia <mail@zarino.co.uk>
2016-12-15 17:27:47 +0000

    Compress css output

--- bin/post_deploy.bash
+++ bin/post_deploy.bash
@@ -2,5 +2,5 @@ cd "$(dirname $BASH_SOURCE)"/..
 
 ruby1.9.1 /usr/bin/bundle install --deployment --binstubs ../gem-bin
 cd $1
-../../gem-bin/sass --update assets/sass:assets/css
+../../gem-bin/sass --update assets/sass:assets/css --style compressed
 ../../gem-bin/jekyll build
