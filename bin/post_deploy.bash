2aee1f7c28685380ccffa34e45ce264596dfe515
Sam Pearson <sam@sgp.me.uk>
2021-12-09 12:34:02 +0000

    [Deploy] Simply call to `bundle` in the post-deploy script
    We no longer need to check for specific ruby versions and should remove
    the absolute path to Bundler to allow for local environment changes.

--- bin/post_deploy.bash
+++ bin/post_deploy.bash
@@ -1,10 +1,6 @@
 cd "$(dirname $BASH_SOURCE)"/..
 
-# If 1.9.1 is present, we're on wheezy with 1.8 + 1.9.1 installed, so want to
-# use 1.9.1. If it's not, we'll be on stretch with 2.3 and can call bundle
-# directly.
-RUBY=$(which ruby1.9.1)
-$RUBY /usr/bin/bundle install --deployment --binstubs ../gem-bin
+bundle install --deployment --binstubs ../gem-bin
 cd $1
 ../../gem-bin/sass --update assets/sass:assets/css --style compressed
 ../../gem-bin/jekyll build
