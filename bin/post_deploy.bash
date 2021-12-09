7a2dab96eb61c7aea84707eff39b7463c56af308
Sam Pearson <sam@sgp.me.uk>
2021-12-09 12:36:26 +0000

    [Bundler] Set local deployment via config, not command flag
    Fixes the following deprecation:
    
    [DEPRECATED] The `--deployment` flag is deprecated because it relies on
    being remembered across bundler invocations, which bundler will no
    longer do in future versions. Instead please use `bundle config set
    --local deployment 'true'`, and stop using this flag

--- bin/post_deploy.bash
+++ bin/post_deploy.bash
@@ -1,6 +1,7 @@
 cd "$(dirname $BASH_SOURCE)"/..
 
-bundle install --deployment --binstubs ../gem-bin
+bundle config set --local deployment 'true'
+bundle install --binstubs ../gem-bin
 cd $1
 ../../gem-bin/sass --update assets/sass:assets/css --style compressed
 ../../gem-bin/jekyll build
